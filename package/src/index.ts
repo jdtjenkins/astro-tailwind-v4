ba9f42c5202b2fd200b9c2c939a67b98621f81cc
eveeify <eveeg1971@gmail.com>
2024-03-14 18:58:47 +1100

    chore: added back vite plugins

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,16 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineIntegration } from "astro-integration-kit";
+import { addVitePluginPlugin } from "astro-integration-kit/plugins";
 
 export default defineIntegration({
 	name: "astro-tailwind-v4",
+	plugins: [addVitePluginPlugin],
 	setup: () => {
 		return {
-			"astro:config:setup": ({ logger, updateConfig }) => {
-				updateConfig({
-					vite: {
-						plugins: tailwindcss(),
-					},
-				});
+			"astro:config:setup": ({ logger, addVitePlugin }) => {
+
+				const plugins = tailwindcss()
+
+				for (const plugin of plugins) {
+					addVitePlugin(plugin);
+				}
+
 				logger.info("Loaded"); // Kept for debugging but really you should do an env that enables this when in development not production.
 			},
 		};
