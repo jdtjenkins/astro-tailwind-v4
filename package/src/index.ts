7817c535fd76706d175766aa13bc4c954f36c115
eveeify <eveeg1971@gmail.com>
2024-03-14 17:13:36 +1100

    chore: cleaned up code and made the extension better

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,3 +1,19 @@
-import integration from "./integration.js";
+import tailwindcss from "@tailwindcss/vite";
+import { defineIntegration } from "astro-integration-kit";
 
-export default integration;
+export default defineIntegration({
+	name: "astro-tailwind-v4",
+	setup: () => {
+		return {
+			"astro:config:setup": ({ logger, updateConfig }) => {
+				
+				updateConfig({
+					vite: {
+						plugins: tailwindcss(),
+					}
+				})
+				logger.info("Loaded"); // Kept for debugging but really you should do an env that enables this when in development not production.
+			},
+		};
+	},
+});
